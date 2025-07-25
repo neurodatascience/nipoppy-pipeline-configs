ccbba9db93ad5c2f92cfe38bac618ec607aedf59
Michelle Wang <tomichellewang@gmail.com>
2025-07-24 22:30:36 -0400

    [BUG] Improve tests and fix bug in QSIRecon config (#22)
    * update tests to make them install the pipelines
    
    * fix qsirecon config missing variables
    
    * update tracker tests
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * flake8
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/test_pipelines.py
+++ tests/test_pipelines.py
@@ -1,12 +1,15 @@
 """Test pipeline configurations."""
 
+import io
 import json
+import re
 import warnings
 from pathlib import Path
 
 import pytest
 from conftest import DPATH_PIPELINES, PIPELINE_INFO_AND_TYPE, PIPELINE_INFO_BY_TYPE
 from nipoppy.config.container import ContainerInfo
+from nipoppy.config.main import Config
 from nipoppy.config.pipeline import BidsPipelineConfig, ExtractionPipelineConfig
 from nipoppy.env import PipelineTypeEnum
 from nipoppy.layout import DatasetLayout
@@ -14,10 +17,71 @@ from nipoppy.utils import TEMPLATE_REPLACE_PATTERN
 from nipoppy.workflows import (
     BidsConversionRunner,
     ExtractionRunner,
+    PipelineInstallWorkflow,
     PipelineRunner,
     PipelineTracker,
     PipelineValidateWorkflow,
 )
+
+VARIABLE_REPLACE_PATTERN = re.compile(r"\[\[(.*?)\]\]")
+
+
+@pytest.fixture
+def pipeline_variables(tmp_path: Path) -> dict[str, str]:
+    return {
+        "HEUDICONV_HEURISTIC_FILE": str(tmp_path / "heuristic.py"),
+        "DCM2BIDS_CONFIG_FILE": str(tmp_path / "dcm2bids_config.json"),
+        "FREESURFER_LICENSE_FILE": str(tmp_path / "freesurfer_license.txt"),
+        "TEMPLATEFLOW_HOME": str(tmp_path / "templateflow"),
+    }
+
+
+def _install_pipeline(
+    layout: DatasetLayout,
+    pipeline_name: str,
+    pipeline_version: str,
+    monkeypatch: pytest.MonkeyPatch,
+    pipeline_variables: dict[str, str] = None,
+    pipeline_type: PipelineTypeEnum = PipelineTypeEnum.PROCESSING,
+):
+    if pipeline_variables is None:
+        pipeline_variables = {}
+
+    fpath_pipeline = (
+        DPATH_PIPELINES / pipeline_type.value / f"{pipeline_name}-{pipeline_version}"
+    )
+    paths_to_install = [fpath_pipeline]
+    if pipeline_type == PipelineTypeEnum.EXTRACTION:
+        pipeline_config = ExtractionPipelineConfig(
+            **json.loads((fpath_pipeline / "config.json").read_text())
+        )
+        for info in pipeline_config.PROC_DEPENDENCIES:
+            paths_to_install.append(
+                DPATH_PIPELINES
+                / PipelineTypeEnum.PROCESSING.value
+                / f"{info.NAME}-{info.VERSION}"
+            )
+    for path in paths_to_install:
+        monkeypatch.setattr("sys.stdin", io.StringIO("n"))  # do not install container
+        installer = PipelineInstallWorkflow(dpath_root=layout.dpath_root, source=path)
+        installer.run()
+
+    # set pipeline variables
+    config = Config.load(layout.fpath_config)
+    variables_to_set = {
+        variable: value
+        for variable, value in pipeline_variables.items()
+        if (
+            variable
+            in config.PIPELINE_VARIABLES.get_variables(
+                pipeline_type, pipeline_name, pipeline_version
+            )
+        )
+    }
+    config.PIPELINE_VARIABLES.set_variables(
+        pipeline_type, pipeline_name, pipeline_version, variables_to_set
+    )
+    config.save(layout.fpath_config)
 
 
 @pytest.mark.parametrize("dpath_pipeline", DPATH_PIPELINES.glob("*/*-*"))
@@ -82,19 +146,31 @@ def test_extraction_invocation(fpath_invocation: Path):
 def test_runner(
     pipeline_info: tuple[str, str, str],
     pipeline_type: PipelineTypeEnum,
+    pipeline_variables: dict[str, str],
     single_subject_dataset,
+    monkeypatch: pytest.MonkeyPatch,
 ):
     """Test that pipelines run successfully in "simulate" mode."""
     pipeline_name, pipeline_version, pipeline_step = pipeline_info
     layout, participant_id, session_id = single_subject_dataset
     layout: DatasetLayout
 
+    # install the pipeline + any proc dependencies
+    _install_pipeline(
+        layout=layout,
+        pipeline_name=pipeline_name,
+        pipeline_version=pipeline_version,
+        monkeypatch=monkeypatch,
+        pipeline_variables=pipeline_variables,
+        pipeline_type=pipeline_type,
+    )
+
     runner_class = {
         PipelineTypeEnum.BIDSIFICATION: BidsConversionRunner,
         PipelineTypeEnum.PROCESSING: PipelineRunner,
         PipelineTypeEnum.EXTRACTION: ExtractionRunner,
     }[pipeline_type]
-    runner = runner_class(
+    runner: PipelineRunner = runner_class(
         dpath_root=layout.dpath_root,
         pipeline_name=pipeline_name,
         pipeline_version=pipeline_version,
@@ -113,21 +189,38 @@ def test_runner(
     if (fpath_container := runner.pipeline_config.get_fpath_container()) is not None:
         fpath_container.touch()
 
-    invocation_str, descriptor_str = runner.run_single(
+    descriptor_str, invocation_str = runner.run_single(
         participant_id=participant_id, session_id=session_id
     )
 
     assert TEMPLATE_REPLACE_PATTERN.search(invocation_str) is None
     assert TEMPLATE_REPLACE_PATTERN.search(descriptor_str) is None
+    assert VARIABLE_REPLACE_PATTERN.search(invocation_str) is None
 
 
 @pytest.mark.parametrize(
     "pipeline_info", PIPELINE_INFO_BY_TYPE[PipelineTypeEnum.PROCESSING]
 )
-def test_tracker(pipeline_info, single_subject_dataset):
+def test_tracker(
+    pipeline_info,
+    single_subject_dataset,
+    pipeline_variables: dict[str, str],
+    monkeypatch: pytest.MonkeyPatch,
+):
     pipeline_name, pipeline_version, pipeline_step = pipeline_info
     layout, participant_id, session_id = single_subject_dataset
     layout: DatasetLayout
+
+    # install the pipeline
+    _install_pipeline(
+        layout=layout,
+        pipeline_name=pipeline_name,
+        pipeline_version=pipeline_version,
+        monkeypatch=monkeypatch,
+        pipeline_variables=pipeline_variables,
+        pipeline_type=PipelineTypeEnum.PROCESSING,
+    )
+
     tracker = PipelineTracker(
         dpath_root=layout.dpath_root,
         pipeline_name=pipeline_name,
