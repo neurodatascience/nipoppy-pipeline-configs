822fd66cb65dcf21ea186ad05ed1bea63ce4c307
nikhil153 <nikhil153@gmail.com>
2026-02-06 16:54:51 -0500

    added qsirecon-1.1.1 bundle with dsi_studio and mrtrix workflows

--- tests/conftest.py
+++ tests/conftest.py
@@ -41,6 +41,7 @@ PIPELINE_INFO_BY_TYPE: dict[PipelineTypeEnum, Iterable[Tuple[str, str, str]]] = 
         ("qsiprep", "0.23.0", "default"),
         ("qsiprep", "1.0.1", "default"),
         ("qsiprep", "1.0.2", "default"),
+        ("qsirecon", "1.1.1", "default"),
     ),
     PipelineTypeEnum.EXTRACTION: (
         ("fs_stats", "0.2.1", "default"),
