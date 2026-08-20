b31798d3f4ef674577779fc642446acd18921eb4
Nikhil Bhagwat <nikhil153@users.noreply.github.com>
2026-08-20 15:43:27 -0400

    Apply suggestion from @michellewang
    Co-authored-by: Michelle Wang <tomichellewang@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -43,6 +43,8 @@ PIPELINE_INFO_BY_TYPE: dict[PipelineTypeEnum, Iterable[Tuple[str, str, str]]] = 
         ("qsiprep", "1.0.2", "default"),
         ("qsiprep", "1.1.1", "default"),
         ("qsiprep", "26.0.0", "default"),
+        ("qsirecon", "1.1.1", "dsi_studio_autotrack"),
+        ("qsirecon", "1.1.1", "mrtrix_multishell_msmt_ACT-hsvs"),
     ),
     PipelineTypeEnum.EXTRACTION: (
         ("fs_stats", "0.2.1", "default"),
